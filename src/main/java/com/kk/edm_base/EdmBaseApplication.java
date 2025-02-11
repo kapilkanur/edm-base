85211e45733012bd6f50c3f11163818eed913350
Kapil <kanurkapil@gmail.com>
2025-02-11 08:19:58 +0530

    Initial commit

---
+++ src/main/java/com/kk/edm_base/EdmBaseApplication.java
@@ -0,0 +1,13 @@
+package com.kk.edm_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdmBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdmBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kk/edm_base/EdmBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk.edm_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdmBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
